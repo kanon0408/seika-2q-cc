9b1ee3e54a68aa55d116df2e02ab2a655db56cdb
kanon.sasaki <s222t076@st.kyoto-seika.ac.jp>
2024-07-18 20:33:00 +0900

    変更

--- src/app.js
+++ src/app.js
@@ -27,23 +27,92 @@ const data2 = {
 		  borderWidth: 1
 	    }
 	]
-  };
+};
+
+const data3 = { 
+	labels: [ 	'課題',
+			'就活',
+			'今日すごく暑いな',
+			'早く家に帰りたいな',
+			'ようやく週の半ばだ！'],
+	datasets: [
+		  {
+		
+		  data: [50,50,100,150,60],
+		  borderWidth: 1
+	    }
+	]
+};
+
+const data4 = { 
+	labels: [ 	'課題',
+			'就活',
+			'エアロビクス疲れたな',
+			'早く家に帰りたいな',
+			'明日行けば休みだ！'],
+	datasets: [
+		  {
+		
+		  data: [50,40,100,50,200],
+		  borderWidth: 1
+	    }
+	]
+};
+
+const data5 = { 
+	labels: [ 	'課題',
+			'授業終わったら何しようかな',
+			'明日はアルバイト',
+			'楽しみにしている番組',
+			'華の金曜日'],
+	datasets: [
+		  {
+		
+		  data: [50,40,50,50,200],
+		  borderWidth: 1
+	    }
+	]
+};
+
+let chart;
+function updateChart(data) {
+	if (chart == null) {
+		chart = new Chart(ctx, {
+			type: 'pie',
+			data: data
+		});
+	} else {
+		chart.data = data;
+		chart.update();
+	}
+}
 
 document.getElementById('button-1').onclick = function() {
-new Chart(ctx, {
-	type: 'pie',
-	data: data1
-});
-	window.alert("月曜日");
+	updateChart(data1);
+	
 }
 
 document.getElementById('button-2').onclick = function() {
-new Chart(ctx, {
-	type: 'pie',
-	data: data2
-});
-	window.alert("火曜日");
+	updateChart(data2);
 }
+
+document.getElementById('button-3').onclick = function() {
+	updateChart(data3);
+}
+
+document.getElementById('button-4').onclick = function() {
+	updateChart(data4);
+}
+
+document.getElementById('button-5').onclick = function() {
+	updateChart(data5);
+}
+// document.getElementById('button-2').onclick = function() {
+// new Chart(ctx, {
+// 	type: 'pie',
+// 	data: data2,
+	
+// });
 // new Chart(ctx, {
 // 		type: 'pie',
 // 		data: {
